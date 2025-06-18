d01bc682dc7bad3f42c71f335171f49a8c901d84
Daniel Arango Echeverri <daniel.arango20125@ucaldas.edu.co>
2025-06-18 00:51:05 -0500

    Delete src directory

--- src/config/ollamaConfig.js
+++
@@ -1,7 +0,0 @@
-OLLAMA_URL = process.env.OLLAMA_URL || 'http://localhost:11434'
-DEFAULT_MODEL = process.env.DEFAULT_MODEL
-
-module.exports = {
-    OLLAMA_URL,
-    DEFAULT_MODEL
-}
--- src/routes/deepseekRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const { handleChat } = require('../controllers/deepseekController')
-const router = express.Router()
-
-router.post('/', handleChat)
-
-module.exports = router
